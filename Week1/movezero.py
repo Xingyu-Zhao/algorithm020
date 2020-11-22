9d0ec9d7c3dcaee3e388e00576b15438c3cfe0ca
Steven <841762770@qq.com>
2020-11-22 18:33:33 +0800

    v1

---
+++ Week1/movezero.py
@@ -0,0 +1,14 @@
+def moveZeros(num):
+    j = 0
+    for i in range(len(num)):
+        if num[i] != 0:
+            num[j] = num[i]
+            j += 1
+            if i != j:
+                num[i] = 0
+    return num
+
+print(moveZeros([0,1,0,3,12]))
+
+
+
